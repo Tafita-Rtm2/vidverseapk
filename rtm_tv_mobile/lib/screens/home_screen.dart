e2b0758f72b2735af4cd8a8ee1eec19a36ec6a2d
Tafitaniaina rtm <harenabainina@gmail.com>
2026-05-04 20:36:52 +0300

    Update home_screen.dart

--- rtm_tv_mobile/lib/screens/home_screen.dart
+++ rtm_tv_mobile/lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide CarouselController;
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shimmer/shimmer.dart';
 import '../models/channel.dart';
